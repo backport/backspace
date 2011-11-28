cabf2ca173bbd0cd2b141006c17a5490b9c71683
David Prandzioch <dprandzioch@me.com>
2011-11-28 09:11:29 +0100

    User Model (Read-Only-Exceptions)

--- fuel/app/classes/model/user.php
+++ fuel/app/classes/model/user.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * This Model is for use in reading context only!!
+ * Do not create users with it
+ */
 class Model_User extends \Orm\Model
 {
 
@@ -14,7 +18,21 @@ class Model_User extends \Orm\Model
         "profile_fields",
         "created_at",
     );
-    
     protected static $_table_name = "user";
+
+    public static function save()
+    {
+        throw new \Exception("This Model is for use in reading context only!!");
+    }
+
+    public static function update()
+    {
+        throw new \Exception("This Model is for use in reading context only!!");
+    }
+
+    public static function delete()
+    {
+        throw new \Exception("This Model is for use in reading context only!!");
+    }
 
 }
